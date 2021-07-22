82eba43ac7e791cfd29010635782679edef8a868
Julia-Park <julia.ay.park@gmail.com>
2021-07-22 14:12:11 -0400

    Post Table of Contents assignment

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,5 +4,9 @@ require 'tilt/erubis'
 
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
+  @table_of_contents = []
+  
+  File.foreach("data/toc.txt") { |title| @table_of_contents << title.chomp }
+
   erb :home
 end
