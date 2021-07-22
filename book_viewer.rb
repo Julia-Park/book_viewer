12e188b280293e44c86c18b4bba2672f669d5552
Julia-Park <julia.ay.park@gmail.com>
2021-07-22 14:40:41 -0400

    Post Adding a Chapter Page Assignment

--- book_viewer.rb
+++ book_viewer.rb
@@ -5,8 +5,16 @@ require 'tilt/erubis'
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
   @table_of_contents = []
-  
   File.foreach("data/toc.txt") { |title| @table_of_contents << title.chomp }
 
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @table_of_contents = []
+  File.foreach("data/toc.txt") { |title| @table_of_contents << title.chomp }
+  @chapter = File.readlines("data/chp1.txt").join("<br>")
+  
+  erb :chapter
 end
