b124c6f91256781090cc64bff926de91ff1b1b5a
Julia-Park <julia.ay.park@gmail.com>
2021-07-23 19:07:44 -0400

    Add page navigation in contents; minor tweak in TOC

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,16 +4,17 @@ require 'tilt/erubis'
 
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
+  @number = 0
   @table_of_contents = File.readlines("data/toc.txt")
   
   erb :home
 end
 
 get "/chapters/:number" do
-  number = params[:number]
-  @title = "Chapter #{number}"
+  @number = params[:number].to_i
+  @title = "Chapter #{@number}"
   @table_of_contents = File.readlines("data/toc.txt")
-  @chapter = File.readlines("data/chp#{number}.txt").join("<br>")
+  @chapter = File.readlines("data/chp#{@number}.txt").join("<br>")
   
   erb :chapter
 end
