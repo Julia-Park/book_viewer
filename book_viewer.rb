37e33fa4214361481892fe5e9846968a31eb6d19
Julia-Park <julia.ay.park@gmail.com>
2021-07-23 19:14:20 -0400

    Include full title in chapter; minor menu and TOC format change

--- book_viewer.rb
+++ book_viewer.rb
@@ -12,8 +12,8 @@ end
 
 get "/chapters/:number" do
   @number = params[:number].to_i
-  @title = "Chapter #{@number}"
   @table_of_contents = File.readlines("data/toc.txt")
+  @title = "Chapter #{@number}: #{@table_of_contents[@number - 1]}"
   @chapter = File.readlines("data/chp#{@number}.txt").join("<br>")
   
   erb :chapter
