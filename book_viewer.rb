20a2d11b57116994fd487e1137c8ce945c55e132
Julia-Park <julia.ay.park@gmail.com>
2021-07-23 18:00:46 -0400

    Assignment Route Parameters - add route for chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,17 +4,20 @@ require 'tilt/erubis'
 
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @table_of_contents = []
-  File.foreach("data/toc.txt") { |title| @table_of_contents << title.chomp }
-
+  @table_of_contents = File.readlines("data/toc.txt")
+  
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @table_of_contents = []
-  File.foreach("data/toc.txt") { |title| @table_of_contents << title.chomp }
-  @chapter = File.readlines("data/chp1.txt").join("<br>")
+get "/chapters/:number" do
+  number = params[:number]
+  @title = "Chapter #{number}"
+  @table_of_contents = File.readlines("data/toc.txt")
+  @chapter = File.readlines("data/chp#{number}.txt").join("<br>")
   
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
