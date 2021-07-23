98a14958e6ceb839bd420fede436794eae39efaa
Julia-Park <julia.ay.park@gmail.com>
2021-07-23 19:25:55 -0400

    Add before filter

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,17 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+before do
+  @table_of_contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
   @number = 0
-  @table_of_contents = File.readlines("data/toc.txt")
   
   erb :home
 end
 
 get "/chapters/:number" do
   @number = params[:number].to_i
-  @table_of_contents = File.readlines("data/toc.txt")
   @title = "Chapter #{@number}: #{@table_of_contents[@number - 1]}"
   @chapter = File.readlines("data/chp#{@number}.txt").join("<br>")
   
