adf96096100be6ad4b3b6bd275bf952231f746a4
Julia-Park <julia.ay.park@gmail.com>
2021-07-23 21:26:17 -0400

    Add search page and functionality

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,19 +2,38 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+before do
+  @table_of_contents = File.readlines("data/toc.txt")
+  @number = 0
+end
+
 helpers do
   def in_paragraphs(string)
     string.split("\n\n").inject("") { |text, line| text + "<p>#{line}</p>" }
   end
+
+  def contents(chapter_number)
+    File.read("data/chp#{chapter_number}.txt")
+  end
+
+  def search_chapters(query) # return hash chp#: chp path
+    result = {}
+    return result if query == ""
+    search_regexp = Regexp.new(query, Regexp::IGNORECASE)
+
+    (1..@table_of_contents.size).each do |chapter_num|
+      if @table_of_contents[chapter_num - 1].match?(search_regexp) || contents(chapter_num).match?(search_regexp)
+        result[chapter_num - 1] = "/chapters/#{chapter_num}"
+      end
+    end
+
+    result
+  end
 end
 
-before do
-  @table_of_contents = File.readlines("data/toc.txt")
-end
 
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @number = 0
   
   erb :home
 end
@@ -24,13 +43,16 @@ get "/chapters/:number" do
   redirect "/" unless (1..@table_of_contents.size).cover?(@number)
   
   @title = "Chapter #{@number}: #{@table_of_contents[@number - 1]}"
-  @chapter = File.read("data/chp#{@number}.txt")
+  @chapter = contents(@number)
   
   erb :chapter
 end
 
-get "/show/:name" do
-  params[:name]
+get "/search" do
+  @number = -1
+  @title = "Search"
+  @results = search_chapters(params[:query]) if !params[:query].nil?
+  erb :search
 end
 
 not_found do
